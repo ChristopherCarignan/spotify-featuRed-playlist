eafd76240ae7fea673589fa28e8519bfac1c7a4a
ChristopherCarignan <carignanc@gmail.com>
2021-09-17 23:44:41 +0100

    Restrict genres to available seed names

--- query_playlist.R
+++ query_playlist.R
@@ -8,6 +8,136 @@
 
 query_playlist <- function (plID, token) {
   
+  # list of available Spotify music genre seeds
+  available <- c(
+    "acoustic",
+    "afrobeat",
+    "alt-rock",
+    "alternative",
+    "ambient",
+    "anime",
+    "black-metal",
+    "bluegrass",
+    "blues",
+    "bossanova",
+    "brazil",
+    "breakbeat",
+    "british",
+    "cantopop",
+    "chicago-house",
+    "children",
+    "chill",
+    "classical",
+    "club",
+    "comedy",
+    "country",
+    "dance",
+    "dancehall",
+    "death-metal",
+    "deep-house",
+    "detroit-techno",
+    "disco",
+    "disney",
+    "drum-and-bass",
+    "dub",
+    "dubstep",
+    "edm",
+    "electro",
+    "electronic",
+    "emo",
+    "folk",
+    "forro",
+    "french",
+    "funk",
+    "garage",
+    "german",
+    "gospel",
+    "goth",
+    "grindcore",
+    "groove",
+    "grunge",
+    "guitar",
+    "happy",
+    "hard-rock",
+    "hardcore",
+    "hardstyle",
+    "heavy-metal",
+    "hip-hop",
+    "holidays",
+    "honky-tonk",
+    "house",
+    "idm",
+    "indian",
+    "indie",
+    "indie-pop",
+    "industrial",
+    "iranian",
+    "j-dance",
+    "j-idol",
+    "j-pop",
+    "j-rock",
+    "jazz",
+    "k-pop",
+    "kids",
+    "latin",
+    "latino",
+    "malay",
+    "mandopop",
+    "metal",
+    "metal-misc",
+    "metalcore",
+    "minimal-techno",
+    "movies",
+    "mpb",
+    "new-age",
+    "new-release",
+    "opera",
+    "pagode",
+    "party",
+    "philippines-opm",
+    "piano",
+    "pop",
+    "pop-film",
+    "post-dubstep",
+    "power-pop",
+    "progressive-house",
+    "psych-rock",
+    "punk",
+    "punk-rock",
+    "r-n-b",
+    "rainy-day",
+    "reggae",
+    "reggaeton",
+    "road-trip",
+    "rock",
+    "rock-n-roll",
+    "rockabilly",
+    "romance",
+    "sad",
+    "salsa",
+    "samba",
+    "sertanejo",
+    "show-tunes",
+    "singer-songwriter",
+    "ska",
+    "sleep",
+    "songwriter",
+    "soul",
+    "soundtracks",
+    "spanish",
+    "study",
+    "summer",
+    "swedish",
+    "synth-pop",
+    "tango",
+    "techno",
+    "trance",
+    "trip-hop",
+    "turkish",
+    "work-out",
+    "world-music"
+  )
+  
   # the API URL of the playlist
   playlist <- paste0("https://api.spotify.com/v1/playlists/",plID)
   
@@ -65,13 +195,18 @@ query_playlist <- function (plID, token) {
     info <- jsonlite::fromJSON(rawToChar(req$content))
     
     # add genres
-    for (genre in 1:length(info$genres)) {
-      genres <- c(genres,info$genres[genre])
+    if (length(info$genres)>0) {
+      for (genre in 1:length(info$genres)) {
+        if (info$genres[genre] %in% available) {
+          genres <- c(genres,info$genres[genre])
+        }
+      }
     }
     
     # add popularity
     popularity <- c(popularity,info$popularity)
   }
+  
   
   # retain the top 5 genres
   genres <- names(rev(sort(table(unlist(genres))))[1:5])
